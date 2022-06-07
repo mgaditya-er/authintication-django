fe75c9bf546c1fa18a2757a09ac84758c19d2da8
mgaditya-er <aditya_ghyar@moderncoe.edu.in>
2022-06-07 14:34:06 +0530

    email confirmation complete

--- authform/ecom/App/views.py
+++ authform/ecom/App/views.py
@@ -1,11 +1,18 @@
 from django.contrib import messages
 from django.contrib.auth import logout, authenticate, login
 from django.contrib.auth.models import User
+from django.contrib.sites.shortcuts import get_current_site
+from django.core.mail import send_mail, EmailMessage
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.template.loader import render_to_string
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
 
+from ecom import settings
+from django.utils.encoding import force_bytes
 
 # Create your views here.
+from .tokens import generate_token
 
 
 def home(request):
@@ -21,12 +28,46 @@ def signup(request):
         email = request.POST['email']
         password = request.POST['password']
 
+        if User.objects.filter(username=username):
+            messages.error(request, "Username already exists! ")
+            return redirect('home')
+
+        if User.objects.filter(email=email):
+            messages.error(request, "email already exists! ")
+            return redirect('home')
+
         myuser = User.objects.create_user(username, email, password)
         myuser.first_name = fname
         myuser.first_name = lname
-        # myuser.is_active = False
+        myuser.is_active = False
         myuser.save()
-        messages.success(request, "Your Account Has been successfully Created !")
+        messages.success(request,
+                         "Your Account Has been successfully Created ! \n we have sent you a confirmation mail.\n Please confirm your email in order to activate your account.")
+
+        # Welcome email
+        subject = "Welcome to our ecommerce site !"
+        message = "hello " + myuser.first_name + " !! \n\n " + " wellcome here"
+        from_email = settings.EMAIL_HOST_USER
+        to_list = [myuser.email]
+        send_mail(subject, message, from_email, to_list, fail_silently=True)
+
+        # email Adress Confirmation Email
+        current_site = get_current_site(request)
+        email_subject = "Confirm Your Email @ ecom - Django Login!!"
+        message2 = render_to_string('email_confirmation.html', {
+            'name': myuser.first_name,
+            'domain': current_site.domain,
+            'uid': urlsafe_base64_encode(force_bytes(myuser.pk)),
+            'token': generate_token.make_token(myuser)
+        })
+        email = EmailMessage(
+            email_subject,
+            message2,
+            settings.EMAIL_HOST_USER,
+            [myuser.email],
+        )
+        email.fail_silently = True
+        email.send()
         return redirect('signin')
     return render(request, 'App/signup.html')
 
@@ -35,7 +76,6 @@ def signin(request):
     if request.method == 'POST':
         username = request.POST['username']
         password = request.POST['password']
-
 
         user = authenticate(username=username, password=password)
         print(user)
@@ -53,3 +93,19 @@ def signout(request):
     logout(request)
     messages.success(request, "Logged out Successfully")
     return redirect('home')
+
+
+def activate(request, uidb64, token):
+    try:
+        uid = force_bytes(urlsafe_base64_decode(uidb64))
+        myuser = User.objects.get(pk=uid)
+    except (TypeError,ValueError,OverflowError,User.DorsNotExist):
+        myuser = None
+
+    if myuser is not None and generate_token.check_token(myuser,token):
+        myuser.is_active = True
+        myuser.save()
+        login(request,myuser)
+        return redirect('home')
+    else:
+        return render(request,'activation_fail.html')
---
+++ authform/ecom/ecom/info.py
@@ -0,0 +1,5 @@
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'aditya_ghyar@moderncoe.edu.in'
+EMAIL_HOST_PASSWORD = 'hemlataa'
+EMAIL_PORT = 587
